314996263cc46c2deda52a95144271847d287442
Behruzbek <behruzbek076@gmail.com>
2026-02-19 03:23:12 +0500

    tuzatildi

--- src/Maps.jsx
+++ src/Maps.jsx
@@ -3,7 +3,7 @@ import { MapContainer, TileLayer, Marker, Popup, useMap } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 import L from "leaflet";
 import { collection, getDocs, addDoc } from "firebase/firestore";
-import { db } from "./Firebase";
+import { db } from "./firebase";
 
 // Marker piktogrammalari
 const greenIcon = new L.Icon({ iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-green.png', shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png', iconSize: [25, 41], iconAnchor: [12, 41], popupAnchor: [1, -34], shadowSize: [41, 41] });
